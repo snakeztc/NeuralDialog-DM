19bc1e814c84276ba900d6510f83c52b0137a8f6
snakeztc <zhaotiancheng.hz@gmail.com>
2016-03-23 14:09:32 -0400

    final push for slot LSTM

--- Utils/config.py
+++ Utils/config.py
@@ -3,7 +3,7 @@ corpus_path = root_path + 'Data/top100.json'
 action_path = root_path + 'Data/action_data.p'
 model_dir = root_path + 'Models/'
 
-generalConfig = {"global_seed": 2000,
+generalConfig = {"global_seed": 100,
                  "greedy_temp": 0.5,
                  "corpus_size": 100,
                  "q_verbal": False,
@@ -25,18 +25,9 @@ slotConfig = {"loss_reward": -30.0,
               "wrong_guess_reward": -5.0,
               "logic_error": -10.0,
               "episode_cap": 100,
-              "max_inform": 20,
+              "max_inform": 10,
               "discount_factor": 0.99}
 
-# The idea of this simulator is remove the need for slot filling
-# just remember the database via a neural network
-end2endConfig = {"loss_reward": -30.0,
-                 "win_reward": 30.0,
-                 "step_reward": 0.0,
-                 "wrong_guess_reward": -10.0,
-                 "logic_error": -5.0,
-                 "episode_cap": 40,
-                 "discount_factor": 0.99}
 # Oracle State
 dqnConfig = {"test_interval": 5000,
              "max_sample": 150001,
@@ -49,10 +40,10 @@ dqnConfig = {"test_interval": 5000,
              "update_frequency": 4,
              "test_trial": 200,
              "doubleDQN": True,
-             "l1-share": 256,
+             "l1-share": 700,
              "l1-verbal": 256,
              "l2-verbal": 128,
-             "l1-computer": 100,
+             "l1-computer": 256,
              "l2-computer": 128,
              "third_hidden": None,
              "dropout": 0.3}
